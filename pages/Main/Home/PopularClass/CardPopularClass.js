f0fde423fafb27a6ef4bbdcd82e48756784bcf8e
Jonathan Tri <jonathantric@gmail.com>
2022-08-30 10:11:25 +0700

    fix run on real device, fix popular class card responsive

--- pages/Main/Home/PopularClass/CardPopularClass.js
+++ pages/Main/Home/PopularClass/CardPopularClass.js
@@ -1,6 +1,6 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {View, Text, Image, Flex, Box, Spacer} from 'native-base';
+import {View, Text, Image, Flex, Box, Spacer, HStack} from 'native-base';
 import colors from '../../../../assets/colors/colors';
 import {Pressable} from 'react-native';
 
@@ -24,7 +24,7 @@ export default function CardPopularClass({
         source={thumbClass}
         style={{width: 128, height: 162, resizeMode: 'cover'}}
       />
-      <Box mx="16px" mt="24px" mb="25.5px">
+      <Box mx="16px" mt="24px" mb="25.5px" width="185px">
         <Text fontSize="10px" fontWeight={500} color={colors.blue}>
           {category}
         </Text>
@@ -32,8 +32,7 @@ export default function CardPopularClass({
           fontSize="14px"
           fontWeight={500}
           color={colors.black}
-          numberOfLines={2}
-          maxWidth="180px">
+          numberOfLines={2}>
           {nameClass}
         </Text>
         <Flex
@@ -57,29 +56,26 @@ export default function CardPopularClass({
 
         <Spacer />
 
-        <Flex
-          mt="17.5px"
-          flexDirection={'row'}
-          justifyContent={'space-between'}
-          alignItems={'center'}>
-          <Flex flexDirection={'row'} justifyContent={'flex-start'}>
-            <Text
-              fontSize="10px"
-              fontWeight={500}
-              mr={'4px'}
-              style={{alignSelf: 'flex-start'}}
-              color={colors.blue}>
-              $
-            </Text>
-            <Text
-              fontSize="14px"
-              fontWeight={500}
-              style={{alignSelf: 'flex-start'}}
-              color={colors.blue}>
-              {price}
-            </Text>
-          </Flex>
-
+        <HStack alignItems={'center'} justifyContent={'space-between'}>
+          <View>
+            <Flex flexDirection={'row'} justifyContent={'flex-start'}>
+              <Text
+                fontSize="10px"
+                fontWeight={500}
+                mr={'4px'}
+                style={{alignSelf: 'flex-start'}}
+                color={colors.blue}>
+                $
+              </Text>
+              <Text
+                fontSize="14px"
+                fontWeight={500}
+                style={{alignSelf: 'flex-start'}}
+                color={colors.blue}>
+                {price}
+              </Text>
+            </Flex>
+          </View>
           <Pressable onPress={() => console.log('pressed')}>
             <Image
               alt="bookmark"
@@ -88,7 +84,7 @@ export default function CardPopularClass({
               source={require('../../../../assets/icons/ic_bookmark.png')}
             />
           </Pressable>
-        </Flex>
+        </HStack>
       </Box>
     </Flex>
   );
